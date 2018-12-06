ca7678ce54a77c9e81d002311459d69f8b8551b1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-06 17:12:26 +0900

    #3.3 GlobalStyles and Header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
+import GlobalStyles from "Components/GlobalStyles";
 
 class App extends Component {
   render() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,18 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Header = styled.header``;
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
 
 const List = styled.ul`
   display: flex;
-  &:hover {
-    background-color: blue;
-  }
 `;
 
-const Item = styled.li``;
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+`;
 
-const SLink = styled(Link)``;
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default () => (
   <Header>
