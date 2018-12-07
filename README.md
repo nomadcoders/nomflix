9903a19086d429de24edee1c57a7d47e6d3962a4
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-07 12:33:25 +0900

    #4.2 API Verbs part One

--- README.md
+++ README.md
@@ -11,8 +11,10 @@ Learning React and ES6 by building a Movie Discovery App.
 
 ## API Verbs
 
-- [ ] Now playing (Movie)
-- [ ] Upcoming (Movie)
-- [ ] Top Rated (TV, Movie)
-- [ ] Popular (TV, Movie)
-- [ ] Airing Today (TV)
+- [x] Now playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV)
+- [x] Popular (TV, X)
+- [x] Airing Today (TV)
+- [ ] TV Show Detail
+- [ ] Movie Detail
--- src/api.js
+++ src/api.js
@@ -8,4 +8,14 @@ const api = axios.create({
   }
 });
 
-export default api;
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular")
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today")
+};
