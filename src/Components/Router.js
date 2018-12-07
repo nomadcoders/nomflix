06bda12b8fef3ec9edfc761ee8675516dd7a4568
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-07 15:08:18 +0900

    #5.4 Search Container

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -9,6 +9,7 @@ import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Header from "Components/Header";
 import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 export default () => (
   <Router>
@@ -18,6 +19,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" component={TV} />
         <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { moviesApi, tvApi } from "../../api";
 
 export default class extends React.Component {
   state = {
@@ -8,6 +9,34 @@ export default class extends React.Component {
     searchTerm: "",
     loading: false,
     error: null
+  };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults }
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults }
+      } = await tvApi.search(searchTerm);
+      this.setState({
+        movieResults,
+        tvResults
+      });
+    } catch {
+      this.setState({ error: "Can't find results." });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
@@ -19,6 +48,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
