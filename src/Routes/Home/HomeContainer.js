01fba11142f4c8b8e874a2e65a530dc20436e20f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-07 13:35:59 +0900

    #5.0 Container Presenter Pattern part One

---
+++ src/Routes/Home/HomeContainer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+
+export default class extends React.Component {
+  state = {
+    nowPlaying: null,
+    upcoming: null,
+    popular: null,
+    error: null,
+    loading: true
+  };
+
+  render() {
+    const { nowPlaying, upcoming, popular, error, loading } = this.state;
+    return (
+      <HomePresenter
+        nowPlaying={nowPlaying}
+        upcoming={upcoming}
+        popular={popular}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
+}
