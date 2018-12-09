fd686892c61958bbc6134019a5f44f87425ced2b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-09 16:02:18 +0900

    Updates

---
+++ precache-manifest.429d5987b59b5e7f91f611a81508e701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "470fcab624058cdab8f5",
+    "url": "/nomflix/static/js/main.470fcab6.chunk.js"
+  },
+  {
+    "revision": "886cf7dff88fdd74e869",
+    "url": "/nomflix/static/js/1.886cf7df.chunk.js"
+  },
+  {
+    "revision": "621cdbff6339be2fca9f",
+    "url": "/nomflix/static/js/runtime~main.621cdbff.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/nomflix/static/media/noPosterSmall.87a0eb6a.png"
+  },
+  {
+    "revision": "9451c1e59f799b65bcd040a505fa0b5c",
+    "url": "/nomflix/index.html"
+  }
+];
